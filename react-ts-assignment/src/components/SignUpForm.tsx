f69eede4018aa6d6e4123215b64af837f9f0ce40
Srivatsak2002 <srivatsa4123@gmail.com>
2024-04-18 14:55:55 +0530

    localstorage array added

--- react-ts-assignment/src/components/SignUpForm.tsx
+++ react-ts-assignment/src/components/SignUpForm.tsx
@@ -182,8 +182,14 @@ export default function SignUpForm() {
   const [formData, setFormData] = React.useState("");
   
   function handleSubmit() {
-    console.log(formData);
-    localStorage.setItem("inputvalue", JSON.stringify(formData));
+    
+    const arr = JSON.parse(localStorage.getItem('formSubmissions') || '[]');
+
+    arr.push(formData);
+
+    localStorage.setItem("formSubmissions", JSON.stringify(arr));
+
+    console.log(arr);
   }
   
   return (
