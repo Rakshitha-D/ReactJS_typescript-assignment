bbd8b7bbf68068554c036e135b8f14201adf90e4
Rakshitha.D <rakshithagowda.d@gmail.com>
2024-04-18 23:53:09 +0530

    updated list of users

--- react-ts-assignment/src/components/UserList.tsx
+++ react-ts-assignment/src/components/UserList.tsx
@@ -1,5 +1,16 @@
 import { useState, useEffect } from "react";
 import { getUsers } from "./LocalStorage";
+import {
+  Avatar,
+  IconButton,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import "./UserList.css";
+import { stringAvatar } from "./AvatarFunctions";
 
 export default function UserList() {
   const [usersList, setUsersList] = useState([]);
@@ -8,12 +19,24 @@ export default function UserList() {
     setUsersList(users);
   }, []);
   return (
-    <div>
-      <ul>
-        {usersList.map((user: any) => (
-          <li>{user.personal_details.name}</li>
-        ))}
-      </ul>
+    <div className="usersList">
+      <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+        List of Registered Users
+      </Typography>
+      {usersList.map((user: any) => (
+        <ListItem
+          secondaryAction={
+            <IconButton edge="end" aria-label="delete">
+              <DeleteIcon />
+            </IconButton>
+          }
+        >
+          <ListItemAvatar>
+            <Avatar {...stringAvatar(user.personal_details.name)}></Avatar>
+          </ListItemAvatar>
+          <ListItemText primary={user.personal_details.name} />
+        </ListItem>
+      ))}
     </div>
   );
 }
