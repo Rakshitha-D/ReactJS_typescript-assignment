d1afe3ec83fbfbe9de714e093fcdd722681eff90
Rakshitha.D <rakshithagowda.d@gmail.com>
2024-04-18 19:13:45 +0530

    added registered users list

--- react-ts-assignment/src/routes/Root.tsx
+++ react-ts-assignment/src/routes/Root.tsx
@@ -3,8 +3,6 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
 import { useNavigate } from "react-router-dom";
 import UserList from "../components/UserList";
 
@@ -18,15 +16,6 @@ export default function Root() {
       <Box sx={{ flexGrow: 1 }}>
         <AppBar position="static">
           <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              <MenuIcon />
-            </IconButton>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
               Home
             </Typography>
@@ -37,7 +26,6 @@ export default function Root() {
         </AppBar>
       </Box>
       <UserList />
-      {/*<SignUpForm />*/}
     </div>
   );
 }
