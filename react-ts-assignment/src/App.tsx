ec3cc071bfb9651a698f05dce1b915d4edce88e2
Rakshitha.D <rakshithagowda.d@gmail.com>
2024-04-18 10:08:57 +0530

    added navbar

--- react-ts-assignment/src/App.tsx
+++ react-ts-assignment/src/App.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import SignUpForm from "./components/SignUpForm";
 import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./routes/Root";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <SignUpForm />,
+    element: <Root />,
   },
 ]);
 
